4772a71bc4487ae9cd9d3280eae64018932541be
lelopez02 <lop20027@byui.edu>
2024-02-13 19:10:03 -0300

    task w04

--- scripts/example.js
+++ scripts/example.js
@@ -1,6 +1,3 @@
-/* LESSON 3 - Programming Tasks */
-
-/* Profile Object  */
 let myProfile = {};
 
 myProfile.name = "Leandro Lopez";
@@ -10,8 +7,6 @@ myProfile.photo = 'images/image-portal.png';
 myProfile.favoriteFoods = ["Pizza", "Milanesa", "Ice cream", "Asado", "Flan", "Chocolat Volcano"];
 
 myProfile.hobbies = ["Reading", "Soccer", "Movies"];
-
-/* Populate Profile Object with placesLive objects */
 
 myProfile.placesLived = [];
 
@@ -24,19 +19,12 @@ myProfile.placesLived.push({
     place: "Buenos Aires",
     length: "6 years"
 });
-/* DOM Manipulation - Output */
-
-/* Name */
 
 document.getElementById("name").textContent = myProfile.name;
-
-/* Photo with attributes */
 
 document.getElementById("photo").src = myProfile.photo;
 
 document.getElementById("photo").alt = myProfile.name;
-
-/* Favorite Foods List*/
 
 myProfile.favoriteFoods.forEach(food => {
 
@@ -47,15 +35,11 @@ myProfile.favoriteFoods.forEach(food => {
     document.getElementById("favorite-foods").appendChild(li);
 });
 
-/* Hobbies List */
-
 myProfile.hobbies.forEach(hobby => {
     let li = document.createElement("li");
     li.textContent = hobby;
     document.getElementById("hobbies").appendChild(li);
 });
-
-/* Places Lived DataList */
 
 myProfile.placesLived.forEach(place => {
 
